50e351ab9515a28496be1408168b0d0454f6a31f
Eric Portis <e@ericportis.com>
2022-04-22 23:05:03 -0700

    Add webmention <link>s

--- _includes/base.11ty.js
+++ _includes/base.11ty.js
@@ -6,14 +6,16 @@ exports.render = function(data) {
 return `
 <!doctype html>
 <html lang="en">
-	<head>
+<head>
 	<meta charset="utf-8">
 	<title>${data.title}</title>
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="/css/base.css">
+	<link rel="webmention" href="https://webmention.ericportis.com" />
+	<link rel="me" href="https://twitter.com/etportis" />
+	<link rel="me" href="https://github.com/eeeps" />
 </head>
 <body>
-<h1>header</h1>
 ${data.content}
 </body>
 </html>`;
