b2676f9de1149db0126eff8455dc2a6871c238ca
Eric Portis <e@ericportis.com>
2022-04-26 13:02:08 -0700

    More work on likes microformats, pagination...

--- likes/index.11ty.js
+++ likes/index.11ty.js
@@ -1,17 +1,37 @@
 exports.data = {
-	"layout": "base.11ty.js"
+	"layout": "base.11ty.js",
+	"eleventyExcludeFromCollections": true,
+	"pagination": {
+		"data": "collections.like",
+		"size": 3,
+		"alias": "likes"
+	}
 };
 
 exports.render = function(data) {
 
-	return `
-${ data.likes.reverse().map( ( like, index ) => {
+	const prettyDate = function( date ) {
+		const dayNumber = date.getDate();
+		const year = date.getFullYear();
+		const monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'][ date.getMonth() ];
+		return `${ monthName } ${ dayNumber }, ${ year }`
+	}
 
-	const title = like.title || like.url;
-	let authors = false;
-	if ( like.authors ) {
-		authors = ` by ${
-			like.authors.map( a => {
+	return `
+<main class="h-feed">
+<h1>Likes</h1>
+${ data.likes
+		.sort( ( a, b ) => a.data.date - b.data.date )
+		.map( ( like, index ) => {
+
+		console.log(like.data.url, like.date);
+
+		const title = like.data.metadata?.title || like.url;
+
+		let authors = false;
+		if ( like.data.metadata?.authors ) {
+	
+			const authorsArray = like.data.metadata.authors.map( a => {
 				if ( a.url && a.name ) {
 					return `<a href="${ a.url }">${ a.name }</a>`
 				} else if ( a.name ) {
@@ -19,17 +39,25 @@ ${ data.likes.reverse().map( ( like, index ) => {
 				} else if ( a.url ) {
 					return `<a href="${ a.url }">${ a.url }</a>`
 				}
-			} ).join(', ')
-		}`;
-	}
-	return `
-<article>
-	<time>${ like.timestamp }</time>
-	<a href="${ like.url }">${ title }</a>${ authors ? authors : '' }
+			} );
+		
+			if ( authorsArray.length > 1 ) {
+				authorsArray[ authorsArray.length - 1 ] = ` and ${ authorsArray[ authorsArray.length - 1 ] }`
+			}
+		
+			authors = ` by ${ authorsArray.join(', ') }`;
+		
+		}
+	
+		return `
+<article class="h-entry">
+	<header>
+		<a href="${ like.url }">★</a> <time class="dt-published">${ prettyDate( like.date ) }</time>
+	</header>
+	<a class="u-like-of" href="${ like.data.url }">${ title }</a>${ authors ? authors : '' }
 </article>
-`;
+`; } ).join("\n") }
 
-} ).join("\n") }
-`;
+</main>`;
 
 };
