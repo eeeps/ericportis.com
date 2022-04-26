cd7d64618c6ea90e13c3e40fc37f03bdbc455892
Eric Portis <e@ericportis.com>
2022-04-25 19:49:49 -0700

    Fleshing out likes a bit

--- likes/index.11ty.js
+++ likes/index.11ty.js
@@ -7,10 +7,25 @@ exports.render = function(data) {
 	return `
 ${ data.likes.reverse().map( ( like, index ) => {
 
+	const title = like.title || like.url;
+	let authors = false;
+	if ( like.authors ) {
+		authors = ` by ${
+			like.authors.map( a => {
+				if ( a.url && a.name ) {
+					return `<a href="${ a.url }">${ a.name }</a>`
+				} else if ( a.name ) {
+					return a.name;
+				} else if ( a.url ) {
+					return `<a href="${ a.url }">${ a.url }</a>`
+				}
+			} ).join(', ')
+		}`;
+	}
 	return `
 <article>
 	<time>${ like.timestamp }</time>
-	<a href="${ like.url }">${ like.url }</a>
+	<a href="${ like.url }">${ title }</a>${ authors ? authors : '' }
 </article>
 `;
 
