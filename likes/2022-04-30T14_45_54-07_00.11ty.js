71cc1cd61a065fe68179ad5623293962375e61d4
Eric Portis <e@ericportis.com>
2022-04-30 14:45:54 -0700

    like posted by Shortcuts

---
+++ likes/2022-04-30T14_45_54-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://twitter.com/EliFitch/status/1520507232605122561",
+	"date": "2022-04-30T14:45:54-07:00",
+	"metadata": {
+		"title": "Eliacocca on Twitter: \"Lmao the Miami GP harbor is just a jpeg\" / Twitter"
+	}
+};
