9611bb35ee96395f0bc35a139b0914f78ed0d571
Eric Portis <e@ericportis.com>
2022-05-04 16:41:18 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-04T16_41_17-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://daverupert.com/2022/05/notes-from-a-gopher-site/",
+	"date": "2022-05-04T16:41:17-07:00",
+	"metadata": {
+		"title": "Notes from a gopher:// site - daverupert.com"
+	}
+};
