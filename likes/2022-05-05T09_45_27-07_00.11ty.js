91405306223db9a8771eefc218156dc40f51d577
Eric Portis <e@ericportis.com>
2022-05-05 09:45:27 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-05T09_45_27-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://www.scientificamerican.com/article/nose-spray-vaccines-could-quash-covid-virus-variants/",
+	"date": "2022-05-05T09:45:27-07:00",
+	"metadata": {
+		"title": "Nose Spray Vaccines Could Quash COVID Virus Variants - Scientific American"
+	}
+};
