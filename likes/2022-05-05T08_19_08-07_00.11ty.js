d543e11eae85356d372ba8e3d7c0eb530e0b3d23
Eric Portis <e@ericportis.com>
2022-05-05 08:19:08 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-05T08_19_08-07_00.11ty.js
@@ -0,0 +1,12 @@
+exports.data = {
+	"url": "https://twitter.com/WriteArthur/status/1521987954994192384",
+	"date": "2022-05-05T08:19:08-07:00",
+	"metadata": {
+		"title": "a tweet",
+		"authors": [
+			{
+				"name": "@WriteArthur"
+			}
+		]
+	}
+};
