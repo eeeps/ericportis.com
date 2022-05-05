b92d62afd53767d1aad67bc649fa3bcd8d30aa6c
Eric Portis <e@ericportis.com>
2022-05-05 09:16:38 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-05T09_16_37-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://www.apple.com/newsroom/2022/05/apple-google-and-microsoft-commit-to-expanded-support-for-fido-standard/",
+	"date": "2022-05-05T09:16:37-07:00",
+	"metadata": {
+		"title": "Apple, Google, and Microsoft commit to expanded support for FIDO standard - Apple"
+	}
+};
