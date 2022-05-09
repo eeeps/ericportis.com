485619eead36ebb6c7d27cd1f5bd7f96a2066fd3
Eric Portis <e@ericportis.com>
2022-05-09 15:41:35 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-09T15_41_34-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://brainbaking.com/post/2021/05/beyond-webmention-io/",
+	"date": "2022-05-09T15:41:34-07:00",
+	"metadata": {
+		"title": "Host your own webmention receiver | Brain Baking"
+	}
+};
