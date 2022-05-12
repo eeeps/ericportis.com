1a34b08c44cd39a7fc857e1d02b495d34a38f190
Eric Portis <e@ericportis.com>
2022-05-12 10:37:45 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-12T10_37_44-07_00.11ty.js
@@ -0,0 +1,12 @@
+exports.data = {
+	"url": "https://twitter.com/chriscoyier/status/1524774726761021440",
+	"date": "2022-05-12T10:37:44-07:00",
+	"metadata": {
+		"title": "a tweet",
+		"authors": [
+			{
+				"name": "@chriscoyier"
+			}
+		]
+	}
+};
