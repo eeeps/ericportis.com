314418d817e85ec5b106ba4e3fbfd384be7e6805
Eric Portis <e@ericportis.com>
2022-05-12 23:46:26 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-12T23_46_26-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://css-tricks.com/generate-a-pull-request-of-static-content-with-a-simple-html-form/",
+	"date": "2022-05-12T23:46:26-07:00",
+	"metadata": {
+		"title": "Generate a Pull Request of Static Content With a Simple HTML Form | CSS-Tricks - CSS-Tricks"
+	}
+};
