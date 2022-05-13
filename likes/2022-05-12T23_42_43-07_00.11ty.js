02621a7eb86669013f881660e8151f768d37cd8d
Eric Portis <e@ericportis.com>
2022-05-12 23:42:44 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-12T23_42_43-07_00.11ty.js
@@ -0,0 +1,12 @@
+exports.data = {
+	"url": "https://twitter.com/xdesro/status/1524260630672207872",
+	"date": "2022-05-12T23:42:43-07:00",
+	"metadata": {
+		"title": "a tweet",
+		"authors": [
+			{
+				"name": "@xdesro"
+			}
+		]
+	}
+};
