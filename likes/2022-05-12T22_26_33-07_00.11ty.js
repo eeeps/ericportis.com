29baaa2abb3acb4fb96e80112673c23ff9fb8e9b
Eric Portis <e@ericportis.com>
2022-05-12 22:26:34 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-12T22_26_33-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://www.devever.net/~hl/mildlydynamic",
+	"date": "2022-05-12T22:26:33-07:00",
+	"metadata": {
+		"title": "The Demise of the Mildly Dynamic Website"
+	}
+};
