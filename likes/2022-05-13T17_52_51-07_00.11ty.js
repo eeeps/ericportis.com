b469388ff907d703adedc2088cd9470166b1e226
Eric Portis <e@ericportis.com>
2022-05-13 17:52:51 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-13T17_52_51-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://docs.google.com/document/d/117Roz2RlgXdDl9OiDdkrrd1ahnepFeGzIvTB5S4OY9g/edit#",
+	"date": "2022-05-13T17:52:51-07:00",
+	"metadata": {
+		"title": "Media (Web Almanac 2022) - Google Docs"
+	}
+};
