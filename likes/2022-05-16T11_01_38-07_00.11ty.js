54fc2ad9e66070d406248d33554be702992ffdc8
Eric Portis <e@ericportis.com>
2022-05-16 11:01:40 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-16T11_01_38-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://chromestatus.com/feature/5685965186138112",
+	"date": "2022-05-16T11:01:38-07:00",
+	"metadata": {
+		"title": "performance.measureUserAgentSpecificMemory() - Chrome Platform Status"
+	}
+};
