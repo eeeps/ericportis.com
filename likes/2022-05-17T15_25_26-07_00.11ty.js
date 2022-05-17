42e3295079aa0d6f7e2b17e7acb353fb48041f20
Eric Portis <e@ericportis.com>
2022-05-17 15:25:27 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-17T15_25_26-07_00.11ty.js
@@ -0,0 +1,12 @@
+exports.data = {
+	"url": "https://twitter.com/vruba/status/1521609258869043200",
+	"date": "2022-05-17T15:25:26-07:00",
+	"metadata": {
+		"title": "a tweet",
+		"authors": [
+			{
+				"name": "@vruba"
+			}
+		]
+	}
+};
