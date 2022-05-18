6c3607c8fb650f77f3c9f14e4b6c0e0b62a9faef
Eric Portis <e@ericportis.com>
2022-05-18 12:03:37 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-18T12_03_36-07_00.11ty.js
@@ -0,0 +1,12 @@
+exports.data = {
+	"url": "https://twitter.com/LiveOverflow/status/1526992577211293696",
+	"date": "2022-05-18T12:03:36-07:00",
+	"metadata": {
+		"title": "a tweet",
+		"authors": [
+			{
+				"name": "@LiveOverflow"
+			}
+		]
+	}
+};
