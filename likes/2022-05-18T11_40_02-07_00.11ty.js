41be54eb65091f7d3d4c83c4786a5ecfb5842fdf
Eric Portis <e@ericportis.com>
2022-05-18 11:40:02 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-18T11_40_02-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://leaddev.com/product-management/do-successful-products-prioritize-familiarity-over-uniqueness?utm_term=Autofeed&utm_medium=Social&utm_source=Twitter#Echobox=1652821724",
+	"date": "2022-05-18T11:40:02-07:00",
+	"metadata": {
+		"title": "Do successful products prioritize familiarity over uniqueness? | LeadDev"
+	}
+};
