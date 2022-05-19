0193f23e7f9aeb6bfbe755165dea0a4d8d476f0c
Eric Portis <e@ericportis.com>
2022-05-19 13:05:11 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-19T13_05_10-07_00.11ty.js
@@ -0,0 +1,12 @@
+exports.data = {
+	"url": "https://twitter.com/rsnous/status/1527377514548629504",
+	"date": "2022-05-19T13:05:10-07:00",
+	"metadata": {
+		"title": "a tweet",
+		"authors": [
+			{
+				"name": "@rsnous"
+			}
+		]
+	}
+};
