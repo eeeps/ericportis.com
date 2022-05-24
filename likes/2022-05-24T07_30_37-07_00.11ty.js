4d0e503770fc8a349ec445faf36738369e58ea4c
Eric Portis <e@ericportis.com>
2022-05-24 07:30:37 -0700

    like posted by Shortcuts

---
+++ likes/2022-05-24T07_30_37-07_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://blog.jim-nielsen.com/2022/loading-parsing-executing-javascript/",
+	"date": "2022-05-24T07:30:37-07:00",
+	"metadata": {
+		"title": "Loading, Parsing, and Executing JavaScript in 2022 - Jim Nielsen’s Blog"
+	}
+};
