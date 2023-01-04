aea848b95c4544999095e46ffcf4ebdaf3e80e6a
Eric Portis <e@ericportis.com>
2023-01-04 11:32:45 -0800

    like posted by Shortcuts

---
+++ likes/2023-01-04T11_32_44-08_00.11ty.js
@@ -0,0 +1,7 @@
+exports.data = {
+	"url": "https://www.bannersociety.com/2016/9/2/20840137/buffalo",
+	"date": "2023-01-04T11:32:44-08:00",
+	"metadata": {
+		"title": "BUFFALO - Banner Society"
+	}
+};
